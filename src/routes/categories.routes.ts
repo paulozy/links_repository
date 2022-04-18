429c231ba11a107a689fd30bb3394a18599804da
Paulo Abreu <micaeler00@gmail.com>
2022-04-17 22:10:07 -0300

    feat: add ListCategoriesController to controller route of list categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,16 +1,13 @@
 import { Router } from "express";
-import { CategoriesRepository } from "../modules/links/repositories/CategoriesRepository";
 import { createCategoryController } from "../modules/links/usecases/CreateCategory";
+import { listCategoriesController } from "../modules/links/usecases/ListCategory";
 
 export const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (req, res) => {
   return createCategoryController.handle(req, res);
 });
 
-categoriesRoutes.get("/", (_, res) => {
-  const categories = categoriesRepository.list();
-
-  return res.json(categories);
+categoriesRoutes.get("/", (req, res) => {
+  return listCategoriesController.handle(req, res);
 });
