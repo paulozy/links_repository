1b70131901d2e2c192dc27c360affb6295618efa
Paulo Abreu <micaeler00@gmail.com>
2022-04-19 11:20:31 -0300

    feat: add authenticate usecase

---
+++ src/modules/accounts/usecases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,48 @@
+import { inject, injectable } from "tsyringe";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+
+export interface IRequest {
+  email: string;
+  password: string;
+}
+
+export interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+export class AuthenticateUserUseCase {
+  constructor(
+    @inject("UsersRepository") private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error("email or password incorrect");
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("email or password incorrect");
+    }
+
+    const token = sign({}, process.env.SECRET_KEY as string, {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    return {
+      user,
+      token,
+    };
+  }
+}
